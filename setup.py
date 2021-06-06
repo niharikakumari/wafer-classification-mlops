bfa92e38fdfd0b92ca07cbbcf9ba8bde22854b10
Kumari <niharika.a.kumari@accenture.com>
2021-06-06 14:10:41 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting if the wafer is working (a component of semiconductors) or not, building everything utilising the principles of MLOps',
+    author='niharika_kumari',
+    license='MIT',
+)
